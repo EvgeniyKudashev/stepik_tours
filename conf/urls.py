d3d5716850d1cb587732696fab474ae8464aa234
EvgeniyKudashev <kudahev98@inbox.ru>
2022-04-07 20:35:19 +0300

    Tours week 1 finished

--- conf/urls.py
+++ conf/urls.py
@@ -13,9 +13,16 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
+
 from django.urls import path
 
+from tours.views import main_view, departure_view, tour_view, custom_handler404, custom_handler500
+
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('', main_view, name="main"),
+    path('departure/<str:departure>/', departure_view, name="departure"),
+    path('tour/<int:id>/', tour_view, name="tour")
 ]
+
+handler404 = custom_handler404
+handler500 = custom_handler500
